44ec14f3f5f1762695563e12479dc6714c574301
Connor Leigh-Smith <connor.leigh-smith@superawesome.tv>
2015-08-03 17:00:20 +0100

    Restructured project; generated jar file which is now used in SDK

--- src/main/gen/org/nexage/sourcekit/mraid/Manifest.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package org.nexage.sourcekit.mraid;
-
-/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
-public final class Manifest {
-}
